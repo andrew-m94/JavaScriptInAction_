8948957bf35d939b34151e7f6787f640a85cfb0d
andrew morales <andrew.morales725@gmail.com>
2021-11-08 14:24:47 -0500

    Loops complete

--- worksheet.js
+++ worksheet.js
@@ -3,11 +3,11 @@ console.log(dayOfWeek);
 
 dayOfWeek = 'Friday';
 console.log("I can't wait for " + dayOfWeek);
-
+/*
 let animalInput = prompt('What is your favorite animal?');
 let colorInput = prompt('What is your favorite color?');
 console.log("I've never seen a " + colorInput + ' ' + animalInput);
-
+*/
 let timeOfDay = 1300;
 let meal = 'meal';
 
@@ -41,4 +41,24 @@ else if (randomNumber >= 6 && randomNumber <= 8){
 
 else if (randomNumber >= 9 && randomNumber <= 10){
     console.log('Hendrix');
+}
+
+for (i = 0; i < 7; i++) {
+    console.log('JavasScript is cool!')
+}
+
+let numberCounter = 0;
+for (i = 0; i < 11; i++) {
+    console.log(numberCounter++)
+}
+
+let isOdd = 2;
+for (i = 0; i < 10; i++) {
+    if (isOdd % 2 == 0){
+        console.log('Hello');
+    }
+    else{
+        console.log('goodbye');
+    }
+    isOdd++;
 }
